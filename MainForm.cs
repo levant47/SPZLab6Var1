841190f7adaf00588db98fd13d1ed638700986bd
Anton Bodiak <abodiak@prometheusgroup.com>
2020-12-21 22:36:20 +0200

    Initial commit

---
+++ MainForm.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+
+namespace SPZLab6Var1
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
